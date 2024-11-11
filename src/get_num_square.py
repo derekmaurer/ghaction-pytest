6501ce4eb4ba50a526c013b6d5413c9813237a86
Derek Maurer <derekmaurer@gmail.com>
2024-11-11 14:05:47 -0500

    Update get_num_square.py

--- src/get_num_square.py
+++ src/get_num_square.py
@@ -11,12 +11,7 @@ if num:
 else:
     num = 1
 
-# to set output, print to shell in following syntax
-#print(f"::set-output name=num_squared::{num ** 2}")
-
 squared_number = num ** 2
 
-#os.environ['GITHUB_OUTPUT'] = str( squared_number )
-#print(f"The square of {num} is {squared_number}")
-
+# to set output, print to shell in following syntax
 print(f"num_squared={squared_number}")
